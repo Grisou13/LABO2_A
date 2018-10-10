59c26afad25a71105770896b47408aa0f876d706
Thomas Ricci <grisou13@hotmail.fr>
2018-10-10 13:48:06 +0200

    Initial commit

---
+++ LABO2_A/main.cpp
@@ -0,0 +1,26 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : <XX>
+ Fichier     : main.cpp
+ Auteur(s)   : Thomas Ricci
+ Date        : 26.09.2018
+
+ But         : <TODO>
+
+ Remarque(s) : <TODO>
+
+ Compilateur : g++ (Ubuntu 5.4.0-6ubuntu1~16.04.10)
+ -----------------------------------------------------------------------------------
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+   
+   cout << "Hello world" << endl;
+   return EXIT_SUCCESS;
+}
