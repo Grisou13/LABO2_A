d89fb9ef17909d34f75d5ea1f560cde47302d717
Eric Broutba <eric.bousbaa@hotmail.com>
2018-10-11 15:32:25 +0200

    exercice 2 done

--- LABO_02_A/exercice_2.cpp
+++ LABO_02_A/exercice_2.cpp
@@ -15,12 +15,35 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int ex2() {
+int main() {
   
+   const int HAUTEUR = 8;
+   const int LARGEUR = 8;
+   int nombre;
+   cout << "Choisissez nombre entier: ";
+   cin >> nombre;
    
-   cout << "Hello world" << endl;
+   int nombre_x = (nombre - 1) % LARGEUR;
+   int nombre_y = (nombre - 1) / LARGEUR;
+    
+   int sym_horizontale_y = HAUTEUR - nombre_y - 1;
+   int sym_horizontale_x = nombre_x;
+   cout << "Symetrie horizontale : " << sym_horizontale_y * LARGEUR + sym_horizontale_x + 1 << endl;
+   
+   int sym_verticale_y = nombre_y;
+   int sym_verticale_x = LARGEUR - nombre_x - 1;
+   cout << "Symetrie verticale : " << sym_verticale_y * LARGEUR + sym_verticale_x + 1 << endl;
+   
+   int sym_diag_droite_x = HAUTEUR - nombre_y  - 1;
+   int sym_diag_droite_y = LARGEUR - nombre_x - 1;
+   cout << "Symetrie diagonale de droite : " << sym_diag_droite_y * LARGEUR + sym_diag_droite_x + 1 << endl;
+  
+   cout << "Symetrie diagonale de gauche : " << nombre_x * LARGEUR + nombre_y + 1 << endl;
+   
+   
    return EXIT_SUCCESS;
 }
