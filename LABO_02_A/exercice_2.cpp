d0905ecd525d7797fb87451097bb8226f0ebf8c8
Eric Broutba <eric.bousbaa@hotmail.com>
2018-10-11 15:45:43 +0200

    code cleanup && commenting

--- LABO_02_A/exercice_2.cpp
+++ LABO_02_A/exercice_2.cpp
@@ -1,15 +1,25 @@
 /*
  -----------------------------------------------------------------------------------
- Laboratoire : <XX>
+ Laboratoire : 02_A
  Fichier     : main.cpp
- Auteur(s)   : Thomas Ricci
+ Auteur(s)   : Thomas Ricci, Eric Bousbaa, Thibaud Franchetti
  Date        : 26.09.2018
 
- But         : <TODO>
+ But         : Demande à l'utilisateur d'entrer un numéro de case d'un damier 
+ *             de hauteur et largeur déterminé. Puis, affiche les numéros de cases
+ *             des symétries suivantes :
+ *                - Symétrie verticale
+ *                - Symétrie horizontale
+ *                - Symétrie diagonale gauche
+ *                - Symétrie diagonale droite.
 
- Remarque(s) : <TODO>
+ Remarque(s) : - Aucun contrôle sur la saisie utilisateur n'est faite.
+ *             - Les dimensions du damier peuvent être pair ou impair.
+ *             - Les dimensions du damier doivent être égaux.
 
- Compilateur : g++ (Ubuntu 5.4.0-6ubuntu1~16.04.10)
+ Compilateur : - g++ (Ubuntu 5.4.0-6ubuntu1~16.04.10), 
+ *             - Apple LLVM version 9.0.0 (clang-900.0.39.2),
+ *             - ...
  -----------------------------------------------------------------------------------
  */
 
@@ -21,29 +31,32 @@ using namespace std;
 
 int main() {
   
-   const int HAUTEUR = 8;
-   const int LARGEUR = 8;
+   const int COTE = 8;
+
    int nombre;
    cout << "Choisissez nombre entier: ";
    cin >> nombre;
    
-   int nombre_x = (nombre - 1) % LARGEUR;
-   int nombre_y = (nombre - 1) / LARGEUR;
+   int nombre_x = (nombre - 1) % COTE;
+   int nombre_y = (nombre - 1) / COTE;
     
-   int sym_horizontale_y = HAUTEUR - nombre_y - 1;
+   int sym_horizontale_y = COTE - nombre_y - 1;
    int sym_horizontale_x = nombre_x;
-   cout << "Symetrie horizontale : " << sym_horizontale_y * LARGEUR + sym_horizontale_x + 1 << endl;
+   cout << "Symetrie horizontale : " << sym_horizontale_y * COTE + sym_horizontale_x + 1 << endl;
    
    int sym_verticale_y = nombre_y;
-   int sym_verticale_x = LARGEUR - nombre_x - 1;
-   cout << "Symetrie verticale : " << sym_verticale_y * LARGEUR + sym_verticale_x + 1 << endl;
+   int sym_verticale_x = COTE - nombre_x - 1;
+   cout << "Symetrie verticale : " << sym_verticale_y * COTE + sym_verticale_x + 1 << endl;
    
-   int sym_diag_droite_x = HAUTEUR - nombre_y  - 1;
-   int sym_diag_droite_y = LARGEUR - nombre_x - 1;
-   cout << "Symetrie diagonale de droite : " << sym_diag_droite_y * LARGEUR + sym_diag_droite_x + 1 << endl;
+   int sym_diag_droite_x = COTE - nombre_y  - 1;
+   int sym_diag_droite_y = COTE - nombre_x - 1;
+   cout << "Symetrie diagonale de droite : " 
+        << sym_diag_droite_y * COTE + sym_diag_droite_x + 1 
+        << endl;
   
-   cout << "Symetrie diagonale de gauche : " << nombre_x * LARGEUR + nombre_y + 1 << endl;
-   
+   cout << "Symetrie diagonale de gauche : " 
+        << nombre_x * COTE + nombre_y + 1 
+        << endl;
    
    return EXIT_SUCCESS;
 }
